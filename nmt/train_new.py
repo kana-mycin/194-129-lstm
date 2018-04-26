Wrong dimension shape for multiplying in SkipLSTM

nmt/train_new.py
@@ -11,6 +11,18 @@ from nmt.utils import vocab_utils
 from nmt.train import train
 
 from tensorflow.contrib.rnn import BasicLSTMCell, LSTMStateTuple
+from tensorflow.python.ops import math_ops
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import rnn_cell_impl
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import nn_ops
+
+
+
+_BIAS_VARIABLE_NAME = "bias"
+_WEIGHTS_VARIABLE_NAME = "kernel"
 
 # Bogus class for testing the "custom_cell" code I inserted
 class PrintLSTMCell(BasicLSTMCell):
@@ -25,12 +37,20 @@ class PrintLSTMCell(BasicLSTMCell):
 
 L = 2
 
-class SkipLSTMCell(BasicLSTMCell):
+class SkipLSTMCell(rnn_cell_impl.RNNCell):
     """ Based on the paper http://www.aclweb.org/anthology/D16-1093 """
+    
 
-    def __init__(self, num_units, n_skip=20, **kwargs):
-        super(SkipLSTMCell, self).__init__(num_units, **kwargs)
+    def __init__(self, num_units, forget_bias=1.0, activation=None, reuse=None, n_skip=20, **kwargs):
+        super(SkipLSTMCell, self).__init__(_reuse=reuse)
         self._n_skip = n_skip
+        self._num_units = num_units
+        self._forget_bias = forget_bias
+        self._activation = activation or math_ops.tanh
+        self._output_size = self._num_units + 1
+        self._state_size = (self._num_units, self._num_units, 1)
+
+
 
     def call(self, inputs, state):
         """Run this multi-layer cell on inputs, starting from state.
@@ -39,7 +59,6 @@ class SkipLSTMCell(BasicLSTMCell):
         """
 
         x = [tf.constant(["Running a SkipLSTMCell"])]
-        skip_bool = h_cnt % self._n_skip == 0
 
         
         sigmoid = math_ops.sigmoid
@@ -48,6 +67,8 @@ class SkipLSTMCell(BasicLSTMCell):
         # Parameters of gates are concatenated into one multiply for efficiency.
         # if self._state_is_tuple:
         c, h, h_skip, h_cnt = state
+        skip_bool = h_cnt % self._n_skip == 0
+
         # else:
         #   c, h = array_ops.split(value=state, num_or_size_splits=2, axis=one)
 
@@ -64,6 +85,8 @@ class SkipLSTMCell(BasicLSTMCell):
         # performance improvement. So using those at the cost of readability.
         add = math_ops.add
         multiply = math_ops.multiply
+
+        first = multiply(c, sigmoid(add(f, forget_bias_tensor)))
         new_c = add(multiply(c, sigmoid(add(f, forget_bias_tensor))),
                     multiply(sigmoid(i), self._activation(j)))
 
@@ -78,21 +101,52 @@ class SkipLSTMCell(BasicLSTMCell):
         # outputs, next_state = super(SkipLSTMCell, self).call(inputs, state)
         print_output = LSTMStateTuple(tf.Print(next_state.c, x), tf.Print(next_state.h, x))
         return new_state, print_output
+    @property
+    def output_size(self):
+        # outputs h and z
+        return self._num_units
+
+    @property
+    def state_size(self):
+        # the state is c, h, h_skip, h_cnt
+        return (self._num_units, self._num_units, self._num_units, 1)
+
+    def zero_state(self, batch_size, dtype):
+        c = tf.zeros([batch_size, self._num_units])
+        h = tf.zeros([batch_size, self._num_units])
+        return [h, c, h, 0]
+
+    def build(self, inputs_shape):
+        if inputs_shape[1].value is None:
+          raise ValueError("Expected inputs.shape[-1] to be known, saw shape: %s"
+                           % inputs_shape)
+
+        input_depth = inputs_shape[1].value
+        self._kernel = self.add_variable(
+            _WEIGHTS_VARIABLE_NAME,
+            shape=[input_depth + self._num_units, self._num_units])
+        self._bias = self.add_variable(
+            _BIAS_VARIABLE_NAME,
+            shape=[self._num_units],
+            initializer=init_ops.zeros_initializer(dtype=self.dtype))
+
+        self.built = True
+
 
 out_dir = os.path.join('datasets', 'nmt_data_vi')
 site_prefix = "https://nlp.stanford.edu/projects/nmt/data/"
 
-maybe_download(site_prefix + 'iwslt15.en-vi/train.en', out_dir, 13603614)
-maybe_download(site_prefix + 'iwslt15.en-vi/train.vi', out_dir, 18074646)
+# maybe_download(site_prefix + 'iwslt15.en-vi/train.en', out_dir, 13603614)
+# maybe_download(site_prefix + 'iwslt15.en-vi/train.vi', out_dir, 18074646)
 
-maybe_download(site_prefix + 'iwslt15.en-vi/tst2012.en', out_dir, 140250)
-maybe_download(site_prefix + 'iwslt15.en-vi/tst2012.vi', out_dir, 188396)
+# maybe_download(site_prefix + 'iwslt15.en-vi/tst2012.en', out_dir, 140250)
+# maybe_download(site_prefix + 'iwslt15.en-vi/tst2012.vi', out_dir, 188396)
 
-maybe_download(site_prefix + 'iwslt15.en-vi/tst2013.en', out_dir, 132264)
-maybe_download(site_prefix + 'iwslt15.en-vi/tst2013.vi', out_dir, 183855)
+# maybe_download(site_prefix + 'iwslt15.en-vi/tst2013.en', out_dir, 132264)
+# maybe_download(site_prefix + 'iwslt15.en-vi/tst2013.vi', out_dir, 183855)
 
-maybe_download(site_prefix + 'iwslt15.en-vi/vocab.en', out_dir, 139741)
-maybe_download(site_prefix + 'iwslt15.en-vi/vocab.vi', out_dir, 46767)
+# maybe_download(site_prefix + 'iwslt15.en-vi/vocab.en', out_dir, 139741)
+# maybe_download(site_prefix + 'iwslt15.en-vi/vocab.vi', out_dir, 46767)
 
 def create_standard_hparams(data_path, out_dir):
     